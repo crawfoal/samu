bea444bbc2b316238628282fd8ca1874f46a22cc
Amanda Dolan <amandacrawfordalc@gmail.com>
2019-06-01 21:04:26 -0600

    Fix a couple issues

--- config/prod.exs
+++ config/prod.exs
@@ -71,6 +71,3 @@ config :logger, level: :info
 #     config :samu, SamuWeb.Endpoint, server: true
 #
 
-# Finally import the config/prod.secret.exs
-# which should be versioned separately.
-import_config "prod.secret.exs"
